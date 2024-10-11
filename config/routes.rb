893d63c3aee8a6570c7a619ea0bd168454f1d204
nekoharuki <harukiyamada1212@gmail.com>
2024-10-11 18:28:54 +0900

    getcreate

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,6 @@
 Rails.application.routes.draw do
-  get 'reviews/index'
-  get 'home/top'
-  get 'users/index'
-  get 'items/index'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  get "items/index" => "items#index"
+  get "items/:id" => "items#show"
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  get "/" => "home#top"
 end
