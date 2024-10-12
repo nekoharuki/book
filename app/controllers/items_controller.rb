699f964bf8e6b524f571fb085455e9ed2d13bcc8
nekoharuki <harukiyamada1212@gmail.com>
2024-10-12 13:03:57 +0900

    items変更

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -6,4 +6,52 @@ class ItemsController < ApplicationController
   def show
     @item=Item.find_by(id: params[:id]);
   end
+  def new
+  end
+  def create
+    @item=Item.new(name: params[:name],
+    author: params[:author],
+    publisher: params[:publisher],
+    content: params[:content],
+    situation: params[:situation],
+    category: params[:category],
+    image: params[:image]
+    )
+    if @item.save
+      flash[:notice]="登録できました"
+      redirect_to("/items/index");
+    else
+      render("items/new")
+    end
+  end
+
+
+  def edit
+    @item=Item.find_by(id: params[:id])
+  end
+  def update
+    @item=Item.find_by(id: params[:id])
+    @item.name=params[:name]
+    @item.author=params[:author]
+    @item.publisher=params[:publisher]
+    @item.content=params[:content]
+    @item.situation=params[:situation]
+    @item.category=params[:category]
+    @item.image=params[:image]
+    if @item.save
+      flash[:notice]="編集できました"
+      redirect_to("/items/index");
+    else
+        render("items/edit")
+    end
+  end
+def destroy
+  @item=Item.find_by(id: params[:id])
+  if @item.destroy
+    flash[:notice]="削除できました"
+    redirect_to("/items/index");
+  else
+    render("items/show")
+  end
+end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
   get "items/index" => "items#index"
-  get "items/:id" => "items#show"
+  get "items/new" => "items#new"
+  post "items/create" => "items#create"
+  get "items/:id/edit" => "items#edit"
+  post "items/:id/update" => "items#update"
+  delete "items/:id/destroy" => "items#destroy"
 
+
+  get "items/:id" => "items#show"
   get "/" => "home#top"
 end
