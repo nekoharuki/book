1bbbae2318eb063b52852fb509a0c063ba744fe3
nekoharuki <harukiyamada1212@gmail.com>
2024-10-12 17:05:25 +0900

    userscreate

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,10 @@
 class Item < ApplicationRecord
+  validates :name, {presence: true}
+  validates :author, {presence: true}
+  validates :publisher, {presence: true}
+  validates :content, {presence: true}
+  validates :situation, {presence: true}
+  validates :category, {presence: true}
+  validates :image, {presence: true}
+
 end
