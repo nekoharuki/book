f50650d3f3b4ded711d69a95dae3139d7ff4e733
nekoharuki <harukiyamada1212@gmail.com>
2024-10-12 13:38:25 +0900

    users変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,21 @@
 Rails.application.routes.draw do
+
   get "items/index" => "items#index"
   get "items/new" => "items#new"
   post "items/create" => "items#create"
   get "items/:id/edit" => "items#edit"
   post "items/:id/update" => "items#update"
-  delete "items/:id/destroy" => "items#destroy"
+  get "items/:id/destroy" => "items#destroy"
 
+  get "users/index" => "users#index"
+  get "users/new" => "users#new"
+  post "users/create" => "users#create"
+  get "users/:id/edit" => "users#edit"
+  post "users/:id/update" => "users#update"
+  get "users/:id/destroy" => "users#destroy"
 
+  get "users/:id" => "users#show"
   get "items/:id" => "items#show"
+
   get "/" => "home#top"
 end
