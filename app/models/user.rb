06687df746efacfa475471b8ec7167026e8e1e37
nekoharuki <harukiyamada1212@gmail.com>
2024-10-19 01:51:37 +0900

    user_items

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   has_secure_password
 
   validates :name, {presence: true}
-  validates :email, {presence: true}
+  validates :email, {presence: true,uniqueness: true}
   validates :address, {presence: true}
 
   def items
