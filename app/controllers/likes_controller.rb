5d7f9a3aad6c1851307361e619971b5de7337590
nekoharuki <harukiyamada1212@gmail.com>
2024-10-19 16:15:58 +0900

    like変更

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,23 +2,34 @@ class LikesController < ApplicationController
   before_action :like_not, only: [:create, :destroy]
 
   def create
-    @like = Like.new(item_id: params[:item_id], user_id: params[:user_id])
-    if @like.save
-      flash[:notice] = "いいねできました"
+    like_check=Like.find_by(user_id: params[:user_id],item_id: params[:item_id])
+    if like_check
+      flash[:notice]="もういいねされています"
       redirect_to("/items/index")
     else
-      flash[:notice] = "いいねできませんでした"
-      redirect_to("/items/index")
+      @like = Like.new(item_id: params[:item_id], user_id: params[:user_id])
+      if @like.save
+        flash[:notice] = "いいねできました"
+        redirect_to("/items/index")
+      else
+        flash[:notice] = "いいねできませんでした"
+        redirect_to("/items/index")
+      end
     end
   end
 
   def destroy
     @like = Like.find_by(item_id: params[:item_id], user_id: params[:user_id])
-    if @like && @like.destroy
-      flash[:notice] = "いいねを削除できました"
-      redirect_to("/items/index")
+    if @like
+      if @like.destroy
+        flash[:notice]="いいねを削除できました"
+        redirect_to("/items/index")
+      else
+        flash[:notice]="いいねを削除できませんでした"
+        redirect_to("/items/index")
+      end
     else
-      flash[:notice] = "いいねを削除できませんでした"
+      flash[:notice] = "まだいいねされていません"
       redirect_to("/items/index")
     end
   end
