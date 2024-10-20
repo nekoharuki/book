5cdb7c343d65b014c7fa70cbdd210ea64b237b07
nekoharuki <harukiyamada1212@gmail.com>
2024-10-20 22:05:28 +0900

    写真

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,11 +1,11 @@
 class Item < ApplicationRecord
   mount_uploader :image, ImageUploader
-  validates :name, {presence: true}
-  validates :author, {presence: true}
-  validates :publisher, {presence: true}
+  validates :name, {presence: true,length:{maximum:20}}
+  validates :author, {presence: true,length:{maximum:20}}
+  validates :publisher, {presence: true,length:{maximum:20}}
   validates :content, {presence: true,length:{maximum:500,minimum:30}}
   validates :situation, {presence: true}
-  validates :category, {presence: true}
+  validates :category, {presence: true,length:{maximum:20}}
   validates :user_id, {presence: true}
   validates :image, {presence: true}
 
