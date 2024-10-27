e87950f2f49b9cfd36a0248f69827f14eadd5585
nekoharuki <harukiyamada1212@gmail.com>
2024-10-27 12:27:39 +0900

    a

--- app/models/trade.rb
+++ app/models/trade.rb
@@ -3,7 +3,4 @@ class Trade < ApplicationRecord
   validates :user_offered_id, {presence: true}
   validates :item_requested_id, {presence: true}
   validates :user_requested_id, {presence: true}
-
-  belongs_to :user
-
 end
