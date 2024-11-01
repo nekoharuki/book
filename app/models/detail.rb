f6257ca37b9c584dd65d8feb756fc4d40ea3be58
nekoharuki <harukiyamada1212@gmail.com>
2024-11-01 12:25:00 +0900

    モデル変更

--- app/models/detail.rb
+++ app/models/detail.rb
@@ -14,14 +14,14 @@ class Detail < ApplicationRecord
     return item_offered
   end
 
-  def user_offered
-    user_offered=User.find_by(id: user_offered_id)
-    return user_offered
+  def user_requested
+    user_requested=User.find_by(id: user_requested_id)
+    return user_requested
   end
-  
-  def item_offered
-    item_offered=Item.find_by(id: item_offered_id)
-    return item_offered
+
+  def item_requested
+    item_requested=Item.find_by(id: item_requested_id)
+    return item_requested
   end
 
 end
