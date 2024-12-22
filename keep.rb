68832b2fc19e5f01d8b826fd622661553bc53d49
nekoharuki <harukiyamada1212@gmail.com>
2024-12-22 11:08:56 +0900

    発送できｔｓ

--- keep.rb
+++
@@ -1,7 +0,0 @@
-<% if user_requested_status==1  %>
-<%= link_to "#{item.item_requested.title}を配送できました", "/items/delivery/#{@hashids.encode(item.item_requested.id)}/#{@hashids.encode(item.item_offered.id)}", class: "btn btn-primary mt-2" %>
-<%end%>
-
-<% if user_offered_status==1 %>
-<%= link_to "#{item.item_offered.title}を配送できました", "/items/delivery/#{@hashids.encode(item.item_offered.id)}/#{@hashids.encode(item.item_requested.id)}", class: "btn btn-primary mt-2" %>
-<% end %>
